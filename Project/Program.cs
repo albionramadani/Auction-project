fda4f4fc815a1b7ec41d7ba1401d398e86d44120
albionramadani <albionr9@gmail.com>
2023-12-11 01:43:47 +0100

    added new feature: logging into db

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Project
 {
@@ -7,6 +8,22 @@ namespace Project
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().
+                         AddJsonFile("appsettings.json").Build();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+            try
+            {
+                Log.Information("Application starting here");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
